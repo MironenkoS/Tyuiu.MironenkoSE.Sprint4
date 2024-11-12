9f5de065858f6e7bcedc1a4db29d8d7b251b3e62
664 <stephencurrytopone@gmail.com>
2024-11-12 22:49:19 +0500

    Task 3

---
+++ Tyuiu.MironenkoSE.Sprint4.Task.V18.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MironenkoSE.Sprint4.Task.V18.Lib
+{
+    public class DataService : ISprint4Task3V18
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = 5;
+            int colums = 5;
+
+            int count = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    if (array[i, j] % 2 == 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
+        
+
+       
+    
+
---
+++ Tyuiu.MironenkoSE.Sprint4.Task.V18.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.MironenkoSE.Sprint4.Task.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
