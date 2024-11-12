a5b41af487a887f103bd9da65a70f69d71b362cc
664 <stephencurrytopone@gmail.com>
2024-11-12 22:32:06 +0500

    Task 2

---
+++ Tyuiu.MironenkoSE.Sprint4.Task2.V22.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.MironenkoSE.Sprint4.Task2.V22.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
