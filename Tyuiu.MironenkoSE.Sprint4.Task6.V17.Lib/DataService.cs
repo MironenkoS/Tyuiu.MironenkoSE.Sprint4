5d0c7f10adb93b0b456518e8a27fc85c956d7521
664 <stephencurrytopone@gmail.com>
2024-11-14 18:37:21 +0500

    Task 6

--- Tyuiu.MironenkoSE.Sprint4.Task6.V17.Lib/DataService.cs
+++ Tyuiu.MironenkoSE.Sprint4.Task6.V17.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.MironenkoSE.Sprint4.Task6.V17.Lib
     {
         public int Calculate(string[] array)
         {
-            int count = 0;
+            int count = 1;
             foreach (var item in array)
             {
                 if (item.Length > 7)
